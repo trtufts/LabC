3bcb7c653a5a47de74029ac299603a2790643e85
Timothy Tufts <trtufts@gmail.com>
2013-11-16 17:08:27 -0500

    Added Latches and some assertions

--- TuftsCorreaLabC.c
+++ TuftsCorreaLabC.c
@@ -4,9 +4,29 @@
 #include <string.h>
 #include <assert.h>
 
+enum inst {ADD,ADDI,SUB,MULT,BEQ,LW,SW,HALT};
+
+//Consider changinng to typedef to remove struct code clutter
+struct inst{
+	int op;
+	int s1;
+	int s2;
+	int d;
+	int im;
+};
+
+struct latch{
+	struct inst pipedInst;
+	int validItem;
+}
+
 struct inst iMem[];
 int dMem[];
-int reg[];
+long int reg[];
+struct latch IF_ID;
+struct latch ID_EX;
+struct latch EX_MEM;
+struct latch MEM_WB;
 int totalCycles;
 int IFcycles;
 int IDcycles;
@@ -14,13 +34,8 @@ int EXcycles;
 int MEMcycles;
 int WBcycles;
 int pc;
-
-enum inst {ADD,ADDI,SUB,MULT,BEQ,LW,SW,HALT};
-
-//Consider changinng to typedef to remove struct code clutter
-struct inst{
-	int op, s1, s2, d, im;
-};
+int branchPending;
+int simMode;
 
 void IF(int c){
 //c cycles
@@ -90,10 +105,10 @@ char * progScanner(FILE *inputFile, char * instruction){
 main(int argc, char* argv[]){
 	//instantiate the Mem's
 	iMem = malloc(512*sizeof(struct int)); //1 instruction takes up 1 word, so iMem is an array of 512 instructions
-	dMem = malloc(2048*sizeof(int)); //Both memory arrays are the same size
+	dMem = malloc(2048*sizeof(int)); //Data memory is stored as bytes
 	
 	//instantiate the registers
-	reg = malloc(32*sizeof(int));
+	reg = malloc(32*sizeof(long int));
 	reg[0] = 0;
 
 	//instantiates the utilization data collection variables
@@ -106,18 +121,29 @@ main(int argc, char* argv[]){
 	
 	//instantiates general function variables
 	pc = 0;
+	struct inst tempInst = {0, 0, 0, 0, 0};
+	IF_ID = {tempInst, 0};
+	ID_EX = {tempInst, 0};
+	EX_MEM = {tempInst, 0};
+	MEM_WB = {tempInst, 0};
 	
-	//Parse input to get file name, Mem time c, Multiply time m, EX op time n
+	//Parse inputs to get file name, Mem time c, Multiply time m, EX op time n
 	string inputFileName = argv[argc-4];
 	int c = atoi(argv[argc-3]);
 	int m = atoi(argv[argc-2]);
 	int n = atoi(argv[argc-1)];
 	
+	assert(c>0);	//These three variables should be greater than 0, or else it causes problems later
+	assert(m>0);
+	assert(n>0);
+
+	assert((simMode==0)||(simMode==1));	//There are only two options for simMode, catches if there is an error in this assignment
+	
+	
 	//Instruction Parsing block
 	char * instruction = malloc(100*sizeof(char);
 	string fullInput = strcat("./", inputFileName);
 	FILE * inputFile = fopen(fullInput, "r");
-	struct inst tempInst;
 	do{
 		instruction = progScanner(inputFile, instruction);
 		tempInst = parser(instruction);
