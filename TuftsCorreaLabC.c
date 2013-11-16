9cc4b03c43b59757c7a2e29c3653ee4c42ef1321
Timothy Tufts <trtufts@gmail.com>
2013-11-16 16:22:43 -0500

    More Variable Definitions changes, progScanner overhaul

--- TuftsCorreaLabC.c
+++ TuftsCorreaLabC.c
@@ -4,42 +4,53 @@
 #include <string.h>
 #include <assert.h>
 
-int iMem[] = (int *) malloc(2048*sizeof(int)); //2048 Bytes 
-int dMem[] = (int *) malloc(2048*sizeof(int)); //Both memory arrays are the same size
+struct inst iMem[];
+int dMem[];
+int reg[];
+int totalCycles;
+int IFcycles;
+int IDcycles;
+int EXcycles;
+int MEMcycles;
+int WBcycles;
+int pc;
 
+enum inst {ADD,ADDI,SUB,MULT,BEQ,LW,SW,HALT};
+
+//Consider changinng to typedef to remove struct code clutter
 struct inst{
 	int op, s1, s2, d, im;
-}
+};
 
-IF(int c){
+void IF(int c){
 //c cycles
 }
 
-ID(){
+void ID(){
 //One cycle
 }
 
-EX(int n, int m){
+void EX(int n, int m){
 //n cycles or m cycles for multiply
 }
 
-MEM(int c){
+void MEM(int c){
 //c cycles
 }
 
-WB(){
+void WB(){
 //One cycle
 }
 
 //Needs to pick R or I type, and parse stuff into numbers for iMem
-struct inst parser(String instruction){
+struct inst parser(char * instruction){
 	int i;
 	char delimiters[]=", ";  // Define delimiters for the strtok functions
 	char ** instructionFields; //Define the resulting instruction fields
 	char format;
 
 	//parse first field to find opcode
-	*instructionFields = (char *) malloc(6*sizeof(char*));
+	*instructionFields = malloc(6*sizeof(char*));
 	instructionFields[0] = strtok(instruction, delimiters);
 	//add, sub, mul are R type
 	//addi, lw, sw, beq are I type
@@ -47,7 +58,7 @@ struct inst parser(String instruction){
 	
 	if(format == 'R'){
 		for (i=1; i<4; i++)
-			*(instructionFields+i) = (char *) malloc(20*sizeof(char *));
+			*(instructionFields+i) = malloc(20*sizeof(char *));
 	}
 	else if(format == 'I'){
 	}
@@ -65,44 +76,54 @@ struct inst parser(String instruction){
 	printf("%s \n%s \n%s \n", instructionFields[0], instructionFields[1], instructionFields[2]);
 }
 
-//almost done, needs fgets tuning
-char * progScanner(string inputFile){
-	char *traceEntry;
-	FILE *ifp;
-	string fullInput = "./" + inputFile;
-
-	traceEntry = malloc(200*sizeof(char));
-	
-	ifp = fopen(fullInput, "r");
-
-	while (fgets(traceEntry, 100, ifp)) 
-		parser(traceEntry);
-	fclose(ifp);
+//should be functional
+char * progScanner(FILE *inputFile, char * instruction){
+	if(fgets(instruction, 100, inputFile) != NULL){
+		return instruction;
+	}
+	else{
+		return NULL;
+	}
 }
 
+
 main(int argc, char* argv[]){
-	//instantiate the dMem to all 0's, 2048 bytes to set
+	//instantiate the Mem's
+	iMem = malloc(512*sizeof(struct int)); //1 instruction takes up 1 word, so iMem is an array of 512 instructions
+	dMem = malloc(2048*sizeof(int)); //Both memory arrays are the same size
 	
 	//instantiate the registers
-	int reg[] = (int *) malloc(32*sizeof(int));
+	reg = malloc(32*sizeof(int));
 	reg[0] = 0;
 
 	//instantiates the utilization data collection variables
-	int totalCycles = 0;
-	int IFcycles = 0;
-	int IDcycles = 0;
-	int EXcycles = 0;
-	int MEMcycles = 0;
-	int WBcycles = 0;
+	totalCycles = 0;
+	IFcycles = 0;
+	IDcycles = 0;
+	EXcycles = 0;
+	MEMcycles = 0;
+	WBcycles = 0;
 	
-	
+	//instantiates general function variables
+	pc = 0;
 	
 	//Parse input to get file name, Mem time c, Multiply time m, EX op time n
-	string inputFile = argv[argc-4];
+	string inputFileName = argv[argc-4];
 	int c = atoi(argv[argc-3]);
 	int m = atoi(argv[argc-2]);
 	int n = atoi(argv[argc-1)];
 	
-	progScanner(inputFile);
+	//Instruction Parsing block
+	char * instruction = malloc(100*sizeof(char);
+	string fullInput = strcat("./", inputFileName);
+	FILE * inputFile = fopen(fullInput, "r");
+	struct inst tempInst;
+	do{
+		instruction = progScanner(inputFile, instruction);
+		tempInst = parser(instruction);
+		
+	} while(tempInst.op != 7);
+	fclose(inputFile);
+	
 	
 }
